502410404268b215c522d64491f03e16574588fb
咿云冷雨 <106859907+Numbersf@users.noreply.github.com>
2026-04-16 05:52:39 +0800

    unicode_filter.sh、service.sh加入翻译

--- script/service.sh
+++ script/service.sh
@@ -2,15 +2,11 @@
 MODDIR=${0%/*}
 sleep 32
 
-# 获取系统语言的前缀部分
-LANGUAGE=$(getprop persist.sys.locale | cut -d'-' -f1)
-if [ "$LANGUAGE" = "zh" ]; then
-  # 中文通知
-  su -lp 2000 -c "cmd notification post -S bigtext -t 'MFGA' 'Tag' '感谢使用高级全局字体模块MFGA，滑动以删除本通知。Powered by Yiyunlengyu（咿云冷雨）'"
-else
-  # English notification information
-  su -lp 2000 -c "cmd notification post -S bigtext -t 'MFGA' 'Tag' 'Thank for using Advanced Global Font Module MFGA. Swipe to dismiss this notification. Powered by Yiyunlengyu.'"
-fi
+MODPATH="$MODDIR"
+. "$MODPATH/lang/lang.sh"
+
+# 发送开机通知
+su -lp 2000 -c "cmd notification post -S bigtext -t '$NOTIFY_TITLE' '$NOTIFY_TAG' '$NOTIFY_BODY'"
 
 # 取消部分APP内置字体权限
 find /data/user/0/com.dragon.read/files/font -type f \( -iname "*.ttf" -o -iname "*.otf" -o -iname "*.ttc" \) -exec chmod 000 {} \;
